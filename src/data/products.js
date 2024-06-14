f054415a207b624bb5221026cc9d42ce9b2ec909
Md Alamin <alaminduc516@gmail.com>
2024-06-15 02:25:13 +0600

    next product import and export

---
+++ src/data/products.js
@@ -0,0 +1,5 @@
+export const products = [
+    { id: 1, name: 'Product 1', description: 'Description of Product 1', price: 10.00, stock: 100 },
+    { id: 2, name: 'Product 2', description: 'Description of Product 2', price: 20.00, stock: 50 },
+  ];
+  
